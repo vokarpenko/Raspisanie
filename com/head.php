c0ac8da0a243dd9195a7d313362c5945d3b937a1
AntonKovalev <lornkarios@mail.ru>
2018-11-17 21:43:42 +0300

    Добавил немного стиля

--- com/head.php
+++ com/head.php
@@ -1,8 +1,9 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="\RaspisanieServer\css\my.css">
-
-  	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <link  href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css" >
+  <link  href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
 </head>
 
 
--- template.php
+++ template.php
@@ -8,25 +8,80 @@
 <body>
 <div id="main">
 	<br><br><br>
-    <div id="header"><h1>Find Names</h1></div>
-    <br><br><br>
-    <div id="content">
-    	Преподаватель: 
-    	<input type="text" list="results1" id="searchbox1"/>
-    	<datalist id="results1"></datalist>
-    </div><br><br><br>
-     <div id="content">
-    	Предмет: 
-    	<input type="text" list="results2" id="searchbox2"/>
-    	<datalist id="results2"></datalist>
-    </div><br><br><br>
-    <div id="content">
-    	Группа:
-    	<input type="text" list="results3" id="searchbox3"/>
-    	<datalist id="results3"></datalist>
-    </div>
-
-    
+    <div id="header"><h1>Ввод данных</h1></div>
+    <br>
+    <table  style="	width: 100%;">
+    <tr>
+    	<td style="	width: 50%;">
+		    <div id="content">
+		    	Преподаватель:<br>
+		    	<input type="text" list="results1" id="searchbox1"/>
+		    	<datalist id="results1"></datalist>
+		    </div>
+		    <br>
+		    <br>
+		    <br>
+		    <div id="content">
+		    	Предмет: <br>
+		    	<input type="text" list="results2" id="searchbox2"/>
+		    	<datalist id="results2"></datalist>
+		    </div>
+		    <br>
+		    <br>
+		    <br>
+		    <div id="content">
+		    	Группа:<br>
+		    	<input type="text" list="results3" id="searchbox3"/>
+		    	<datalist id="results3"></datalist>
+		    </div>
+		    <br>
+		    <br>
+		    <br>
+		    <div id="content">
+		    	№ дня:
+		    	<select>
+		    		<option>1</option>
+		    		<option>2</option>
+		    		<option>3</option>
+		    		<option>4</option>
+		    		<option>5</option>
+		    		<option>6</option>
+		    		<option>7</option>
+		    		<option>8</option>
+		    		<option>9</option>
+		    		<option>10</option>
+		    		<option>11</option>
+		    		<option>12</option>
+		    		<option>13</option>
+		    		<option>14</option>
+		    	</select>
+		    </div>
+		    <br>
+		    <br>
+		    <br>
+		    <div id="content">
+		    	№ пары:
+		    	<select>
+		    		<option>1</option>
+		    		<option>2</option>
+		    		<option>3</option>
+		    		<option>4</option>
+		    		<option>5</option>
+		    		<option>6</option>
+		    		<option>7</option>
+		    		<option>8</option>
+		    	</select>
+		    </div>
+		</td>
+		<td>
+		    
+		  		<div class="buttonHolder">
+				  <a href="#" class="button tick"></a>
+				</div>
+			
+		</td>
+    </tr>
+    </table>
 
 
 </div>
