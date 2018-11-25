b5f39f5adba33a4e636f273c803bd7d1806eddf7
AntonKovalev <lornkarios@mail.ru>
2018-11-25 20:49:46 +0300

    viravnivanie

--- js/tableDD.js
+++ js/tableDD.js
@@ -1,20 +1,24 @@
  $( function() {
-    $( "#draggable1, #draggable2" ).draggable({ revert: "invalid" });
-  
+ 	$( "#draggable1, #draggable2" ).draggable({ revert: "invalid" });
+
  	for (var i = 1; i < 50; i++) {
  		$( "#droppable"+i ).droppable({
-      classes: {
-        "ui-droppable-active": "ui-state-active",
-        "ui-droppable-hover": "ui-state-hover"
-      },
-      drop: function( event, ui ) {
-        $( this )
-          /*.addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );*/
-      }
+ 			classes: {
+ 				"ui-droppable-active": "ui-state-active",
+ 				"ui-droppable-hover": "ui-state-hover"
+ 			},
+ 			drop: function( event, ui ) {
+        // $( this )
+       
+        $( ui.draggable).position({
+  			my: "center",
+  			at: "center",
+ 			of: this
+		});
+        	
+    }
 
-    });
+});
  	}
-    
-  } );
+
+ } );
