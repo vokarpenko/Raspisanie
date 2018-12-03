e7c7a19fd03d4e03c0bfbf6da12165f9f9aa98c7
AntonKovalev <lornkarios@mail.ru>
2018-12-03 16:07:21 +0300

    mobile v 0.003

--- m/index.php
+++ m/index.php
@@ -6,7 +6,7 @@ include($_SERVER['DOCUMENT_ROOT']."/m/com/head.php");
 ?>
 
 
-<div class="section">
+<div class="section hero">
 	<div class="container">
 		<div class="row">
 			<div class="one-half column">
