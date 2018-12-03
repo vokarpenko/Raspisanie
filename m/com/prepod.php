6b7c7bf5a7785b3319ecae8fd04e930693078664
AntonKovalev <lornkarios@mail.ru>
2018-12-03 17:13:25 +0300

    mobile v 0.0051

--- m/com/prepod.php
+++ m/com/prepod.php
@@ -3,8 +3,8 @@
 <link rel="stylesheet" type="text/css" href="/m/css/prepod.css">	
 <div class="tabs_box">
     <ul class="tabs_menu">
-        <li><a href="#tab1" class="active">Вкладка 1</a></li>
-        <li><a href="#tab2">Вкладка 2</a></li>
+        <li><a href="#tab1" class="active">Расписание</a></li>
+        <li><a href="#tab2">Пожелания</a></li>
     </ul>
     <div class="tab" id="tab1">
         <p>Контент первой вкладки...</p>
