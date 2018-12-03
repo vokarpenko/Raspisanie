179127a0eb4b8fd9682564f2fe4ba5b88aa72d15
AntonKovalev <lornkarios@mail.ru>
2018-12-03 16:36:29 +0300

    mobile v 0.004

--- m/index.php
+++ m/index.php
@@ -6,17 +6,22 @@ include($_SERVER['DOCUMENT_ROOT']."/m/com/head.php");
 ?>
 
 <body>
-<div class="section hero">
-	<div class="container">
-		<div class="row">
-			<div class="one-half column">
-				<a class="button" href="#">Студент</a>
+	
+	<div class="section hero">
+		<div class="container">
+			<div class="row">
+				<h3 class="hero-heading">Выберите должность.</h3>
 			</div>
-			<div class="one-half column">
-				<a class="button" href="#">Преподаватель</a>
-			</div>
+			<div class="row">
+				<div class="one-half column">
+					<a class="button" href="#">Студент</a>
+				</div>
+				<div class="one-half column">
+					<a class="button" href="#">Преподаватель</a>
+				</div>
 
+			</div>
 		</div>
+
 	</div>
-</div>
 </body>
