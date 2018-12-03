d201f43db082220fe9e55157166ef7ff260f0df8
AntonKovalev <lornkarios@mail.ru>
2018-12-03 15:11:40 +0300

    domain mobile4

--- index.php
+++ index.php
@@ -10,39 +10,43 @@ $db = new MyDB();
 $db->connect();
 	//$_SESSION['db']=$db;
 $is_mobile_device = check_mobile_device();
-if($is_mobile_device){
+if($_SERVER['HTTP_HOST'][0]=='m'){
 	include($_SERVER['DOCUMENT_ROOT']."/m/index.php");
-	
-	// echo "Вы зашли с мобильного устройства";
 }
 else{
-	include($_SERVER['DOCUMENT_ROOT']."/com/head.php");
-	// ГЛАВНЫЙ КОНТРОЛЛЕР
-	if(!empty($_GET['option'])){
-		switch ($_GET['option']) {
-			case "add":
-			include($_SERVER['DOCUMENT_ROOT']."/com/addPar.php");
-			break;
-			case "createRaspisanie":
-			include($_SERVER['DOCUMENT_ROOT']."/com/home.php");
-			break;
-			case "mInfo":
-			include($_SERVER['DOCUMENT_ROOT']."/com/infoTimeTable.php");
-			echo "<timetable>".json_encode($table,JSON_UNESCAPED_UNICODE)."</timetable>";
-			break;
-			case "mWish":
-			include($_SERVER['DOCUMENT_ROOT']."/com/putInfo.php");
-			break;
-			default:
-			include($_SERVER['DOCUMENT_ROOT']."/com/wishes.php");
-			break;
-		}
+	if($is_mobile_device){
+		header('Location: http://m.timetable-fktpm.ru');
+
+	// echo "Вы зашли с мобильного устройства";
 	}
 	else{
-		
-		include($_SERVER['DOCUMENT_ROOT']."/com/wishes.php");
-	}
-}
+		include($_SERVER['DOCUMENT_ROOT']."/com/head.php");
+	// ГЛАВНЫЙ КОНТРОЛЛЕР
+		if(!empty($_GET['option'])){
+			switch ($_GET['option']) {
+				case "add":
+				include($_SERVER['DOCUMENT_ROOT']."/com/addPar.php");
+				break;
+				case "createRaspisanie":
+				include($_SERVER['DOCUMENT_ROOT']."/com/home.php");
+				break;
+				case "mInfo":
+				include($_SERVER['DOCUMENT_ROOT']."/com/infoTimeTable.php");
+				echo "<timetable>".json_encode($table,JSON_UNESCAPED_UNICODE)."</timetable>";
+				break;
+				case "mWish":
+				include($_SERVER['DOCUMENT_ROOT']."/com/putInfo.php");
+				break;
+				default:
+				include($_SERVER['DOCUMENT_ROOT']."/com/wishes.php");
+				break;
+			}
+		}
+		else{
+
+			include($_SERVER['DOCUMENT_ROOT']."/com/wishes.php");
+		}
+	}}
 	// ПОДКЛЮЧЕНИЕ ЯДРА
 
 	//$db->close();
