893f6b192d8489c3c96f847aaa73ba0fb41a18b4
AntonKovalev <lornkarios@mail.ru>
2018-12-03 15:34:20 +0300

    mobile v 0.001

--- m/index.php
+++ m/index.php
@@ -1,5 +1,18 @@
 
 <?php
-echo "HELLO MOBILE!";
+// echo "<pre>HELLO MOBILE!</pre>";
 include($_SERVER['DOCUMENT_ROOT']."/m/com/head.php");
+
 ?>
+
+<div class="three columns"></div>
+<div class="container">
+	<div class="row">
+		<div class="three columns"></div>
+		<div class="three columns"><a class="button" href="#">Студент</a></div>
+		<div class="three columns"><a class="button" href="#">Преподаватель</a></div>
+		<div class="three columns"></div>
+		
+		
+	</div>
+</div>
