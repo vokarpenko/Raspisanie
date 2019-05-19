946d8794b723da11d0fe66c8163a4daa7cb91a31
kovalev <kovalev@aicrobotics.ru>
2019-05-19 19:31:56 +0300

    добавил библу phpmailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -72,6 +72,7 @@ class ComposerStaticInit969964d4f6c9826b67111e4214999c8f
         array (
             'Psr\\SimpleCache\\' => 16,
             'PhpOffice\\PhpSpreadsheet\\' => 25,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'M' => 
         array (
@@ -91,6 +92,10 @@ class ComposerStaticInit969964d4f6c9826b67111e4214999c8f
         'PhpOffice\\PhpSpreadsheet\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Matrix\\' => 
         array (
